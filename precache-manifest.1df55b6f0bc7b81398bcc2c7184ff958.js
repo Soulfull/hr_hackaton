cbd1119ebb8abcef1324747fe15b95f9666589d1
m.smolin <m.smolin@space307.com>
2019-12-19 16:25:28 +0300

    add build

---
+++ precache-manifest.1df55b6f0bc7b81398bcc2c7184ff958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd7be897151054d65d004144566e478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b04ff5260a1dd50353",
+    "url": "/static/css/main.084a3f59.chunk.css"
+  },
+  {
+    "revision": "6609d93cfb0884521cfe",
+    "url": "/static/js/2.4f93c2d1.chunk.js"
+  },
+  {
+    "revision": "85681a160bd973edd675762e042f86e1",
+    "url": "/static/js/2.4f93c2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9b04ff5260a1dd50353",
+    "url": "/static/js/main.fcb0a9b0.chunk.js"
+  },
+  {
+    "revision": "8b731589034df4732de3",
+    "url": "/static/js/runtime-main.e7d358f1.js"
+  },
+  {
+    "revision": "2a0bac72ce885fda8feb519854cac5c5",
+    "url": "/static/media/3floor.2a0bac72.png"
+  }
+]);
